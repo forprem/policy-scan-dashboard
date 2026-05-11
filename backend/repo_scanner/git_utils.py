36d2103c3c53281a0e48959a5ddf9587e5ed9d6e
forprem <forprem@gmail.com>
2026-05-11 09:06:32 -0400

    Update git_utils.py
    for git repo

--- backend/repo_scanner/git_utils.py
+++ backend/repo_scanner/git_utils.py
@@ -4,11 +4,26 @@ import subprocess
 def clone_repo(repo_url: str, pat: str | None):
     temp_dir = tempfile.mkdtemp()
 
-    if pat:
-        repo_url = repo_url.replace("https://", f"https://{pat}@")
+    clone_url = repo_url
+
+    # ------------------------------------------------
+    # Azure DevOps private repo
+    # ------------------------------------------------
+    if "dev.azure.com" in repo_url and pat:
+
+        clone_url = repo_url.replace(
+            "https://",
+            f"https://{pat}@"
+        )
+
+    # ------------------------------------------------
+    # Public GitHub / public git repo
+    # ------------------------------------------------
+    else:
+        clone_url = repo_url
 
     subprocess.run(
-        ["git", "clone", repo_url, temp_dir],
+        ["git", "clone", clone_url, temp_dir],
         check=True
     )
 
