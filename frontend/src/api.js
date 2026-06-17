50253c3e6b6a8c549d468ec3a25c8c9b3e3992a8
forprem <forprem@gmail.com>
2026-06-17 15:04:10 -0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 const API = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: API_BASE_URL
 });
 
 export const scanSite = (url) =>
